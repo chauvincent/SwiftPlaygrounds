3f1153f938ee2c8a07971b1ab9a837c1f724dc23
Vincent Chau <vichau@ucdavis.edu>
2016-03-31 17:45:14 -0700

    added model and utility files

---
+++ Tweetr/Tweetr/JSONParser.swift
@@ -0,0 +1,9 @@
+//
+//  JSONParser.swift
+//  Tweetr
+//
+//  Created by Vincent Chau on 3/31/16.
+//  Copyright © 2016 Vincent Chau. All rights reserved.
+//
+
+import Foundation
---
+++ Tweetr/Tweetr/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Tweetr
+//
+//  Created by Vincent Chau on 3/31/16.
+//  Copyright © 2016 Vincent Chau. All rights reserved.
+//
+
+import Foundation
