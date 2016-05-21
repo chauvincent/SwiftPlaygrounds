088c90cbfbc2b00596e6a3e0f00ddbe6286cd622
Vincent Chau <vichau@ucdavis.edu>
2016-05-21 16:21:17 -0700

    update to GCD playground

--- GCDQueues.playground/Contents.swift
+++ GCDQueues.playground/Contents.swift
@@ -2,4 +2,34 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var welcomeMsg = "Welcome to Vincent's Playground"
+print("GCD REFERSHER IN SWIFT")
+
+func expensiveCall() -> String{
+    print("doing expensive operations")
+    return "Done"
+}
+
+let defaultPriority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+let backgroundQ = dispatch_get_global_queue(defaultPriority, 0)
+
+dispatch_async(backgroundQ, {
+    let result = expensiveCall()
+    dispatch_async(dispatch_get_main_queue(), {
+        print(result)
+    })
+})
+
+// Recall:
+// dispatch_async returns before the block is executed
+// dipatch_sync waits until block is finished before returning.
+// if using sync in inner thread, it doesnt matter when it returns
+// but outer must be async or else main thread will be blocked
+let taskQueue = dispatch_queue_create("TaskQueue", DISPATCH_QUEUE_SERIAL)
+
+dispatch_async(taskQueue, {
+    let results = expensiveCall()
+    dispatch_async(dispatch_get_main_queue(), { 
+        print(results)
+    })
+})
