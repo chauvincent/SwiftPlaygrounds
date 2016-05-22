4281fe5de5c34ebfcd9a82a5bf0a0f6930d88903
Vincent Chau <vichau@ucdavis.edu>
2016-05-21 18:30:03 -0700

    AsyncSerial

--- GCDQueues.playground/Contents.swift
+++ GCDQueues.playground/Contents.swift
@@ -25,7 +25,7 @@ dispatch_async(backgroundQ, {
 // dipatch_sync waits until block is finished before returning.
 // if using sync in inner thread, it doesnt matter when it returns
 // but outer must be async or else main thread will be blocked
-let taskQueue = dispatch_queue_create("TaskQueue", DISPATCH_QUEUE_SERIAL)
+let taskQueue = dispatch_queue_create("com.chau.serial.queue", DISPATCH_QUEUE_SERIAL)
 
 dispatch_async(taskQueue, {
     let results = expensiveCall()
@@ -33,3 +33,43 @@ dispatch_async(taskQueue, {
         print(results)
     })
 })
+
+/*                                      Semaphores/Threads, Flatten completion Blocks
+ 
+    Blocking a thread for an amount of time until a signal from another thread is sent with semaphores
+    Use semaphores when there is an asyc api and need to make synchronous calls, so need to block the aynchronous thread
+*/
+/*
+dispatch_semaphore_t semaphore = dispatch_semaphore_create(0)
+expensiveAsynchronousWork(completionBlock: {
+    dispatch_semaphore_signal(semaphore)
+})
+
+ // Block thread until dispatch_semaphore_signal is called from a different thread(from background)
+ // Never timeout with DISPATCH_TIME_FOREVER
+dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+
+*/
+
+typealias DoneBlock = () -> ()
+typealias JobBlock = (DoneBlock) -> ()
+
+// Create serial queue, enqueue jobs onto the block.
+// JobBlock gives DoneBlock to call when job is done, which will signal the semaphore and allow the serial queue to continue
+class AsyncSerial {
+    private let serialQ = dispatch_queue_create("com.chau.serial.queue2", DISPATCH_QUEUE_SERIAL)
+    
+    func enqueue(job: JobBlock) {
+        dispatch_async(serialQ, {
+          let semaphore = dispatch_semaphore_create(0)
+            job({
+                dispatch_semaphore_signal(semaphore)
+            })
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        })
+    }
+}
+
+
+
+
